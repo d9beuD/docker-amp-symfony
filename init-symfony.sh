830430c75854832dc81db3e580863c9e9d516af8
d9beuD <d9beuD@users.noreply.github.com>
2024-10-28 13:26:52 +0100

    Update init-symfony.sh

--- init-symfony.sh
+++ init-symfony.sh
@@ -1,3 +1,10 @@
+# Init Git
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+# Install Symfony project in new empty directory
 symfony new my-project --version="7.1.*" --webapp
-mv my-project/* .
+
+# Move all files from my-project to root directory
+rsync -a --exclude='.git' my-project/ ./
 rm -rf my-project
